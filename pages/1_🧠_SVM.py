8a2061b63d8c595b4225a989ca5ed1a537e4dc75
Wachixd102 <alonenightcore02@gmail.com>
2026-07-31 14:47:53 +0700

    .

--- pages/1_🧠_SVM.py
+++ pages/1_🧠_SVM.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+import joblib  # ต้องมีบรรทัดนี้!
 import os
 
 st.set_page_config(page_title="SVM Prediction", page_icon="🧠", layout="wide")
